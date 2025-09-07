04adbd2fb013211a539cead5479ce44e1fcd2b86
Lueur3 <sgdm515@gmail.com>
2025-09-07 18:11:23 +0300

    added new buttoms for movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,14 +24,14 @@ class AlienInvasion:
 
     def _check_keydown_events(self, event):
         """Реагирует на нажатие клавиш."""
-        if event.key == pygame.K_RIGHT:
+        if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
             self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
             self.ship.moving_left = True
 
-        if event.key == pygame.K_UP:
+        if event.key == pygame.K_UP or event.key == pygame.K_w:
             self.ship.moving_up = True
-        elif event.key == pygame.K_DOWN:
+        elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
             self.ship.moving_down = True
 
         elif event.key == pygame.K_q:
@@ -39,14 +39,14 @@ class AlienInvasion:
 
     def _check_keyup_events(self, event):
         """Реагирует на опускание клавиш."""
-        if event.key == pygame.K_RIGHT:
+        if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
             self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
             self.ship.moving_left = False
 
-        if event.key == pygame.K_UP:
+        if event.key == pygame.K_UP or event.key == pygame.K_w:
             self.ship.moving_up = False
-        elif event.key == pygame.K_DOWN:
+        elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
             self.ship.moving_down = False
 
     def _check_events(self):
