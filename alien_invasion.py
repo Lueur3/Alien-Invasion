f7ccb1f0fea33cbf0493abdf18a248a1b5b50fcd
Lueur3 <sgdm515@gmail.com>
2025-09-07 16:31:48 +0300

    refactoring funcs

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,20 +17,27 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def _update_screen(self):
+        """Обновляет изображение на экране и отображает на новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            # Отслеживаиние событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+            self._check_events()
 
-            # При каждом проходе цикла перерисовывается экран
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # Отоброжение последнего прорисованного экрана.
-            pygame.display.flip()
+            self._update_screen()
+
+
 
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
