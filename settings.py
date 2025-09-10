f480b6f6d933e359eee2b3eec57b485a2203dc11
Lueur3 <sgdm515@gmail.com>
2025-09-10 19:15:03 +0300

    added increasing alien points by level

--- settings.py
+++ settings.py
@@ -25,6 +25,8 @@ class Settings:
 
         # Темп ускоренной стрельбы
         self.speedup_scale = 1.1
+        # Темп роста стоимости пришельцев
+        self.score_scale = 1.5
 
         self. initialize_dynamic_settings()
 
@@ -43,7 +45,9 @@ class Settings:
         self.curr_rows = 1
 
     def increase_speed(self):
-        """Увеличивает настройки скорости."""
+        """Увеличивает настройки скорости и стоимости пришельца."""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+        if (self.curr_rows - 1) > 0:
+            self.alien_points = int(self.alien_points * self.score_scale)
